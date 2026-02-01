245fab49382ca2605a8a49ca4a5af53aeff5c53b
mattia <mattia.belletti@gmail.com>
2026-02-01 10:15:50 +0100

    feat: enter chat audio

--- components/audio_player/audio_player.gd
+++ components/audio_player/audio_player.gd
@@ -2,11 +2,13 @@ extends Node
 
 
 enum SFX {
-	ProfileSelected
+	ProfileSelected,
+	EnterChat
 }
 
 @onready var _audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 const _profile_selected_audio_stream: AudioStream = preload("uid://ca7lw6qymkt61")
+const _enter_chat_audio_stream: AudioStream = preload("uid://plo7fsj38t2f")
 var _audio_stream_playback_polyphonic: AudioStreamPlaybackPolyphonic
 
 
@@ -18,4 +20,13 @@ func _ready() -> void:
 
 
 func play_sfx(sfx: SFX) -> void:
-	_audio_stream_playback_polyphonic.play_stream(_profile_selected_audio_stream)
+	var audio_stream: AudioStream
+	match sfx:
+		SFX.ProfileSelected:
+			audio_stream = _profile_selected_audio_stream
+		SFX.EnterChat:
+			audio_stream = _enter_chat_audio_stream
+		_:
+			push_warning("Cannot find audio stream for sfx %s" % [sfx])
+			return
+	_audio_stream_playback_polyphonic.play_stream(audio_stream)
