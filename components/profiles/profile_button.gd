df68dc5002fc39a2282e45f87bedb56239960d7b
mattia <mattia.belletti@gmail.com>
2026-02-01 17:49:28 +0100

    feat: final images

--- components/profiles/profile_button.gd
+++ components/profiles/profile_button.gd
@@ -4,14 +4,14 @@ class_name ProfileButton
 
 # All the textures for the special characters
 const _TEXTURES: Array[CompressedTexture2D] = [
-	preload("uid://df0fvqebqiomi"),
-	preload("uid://cgpv2vll6qxhe"),
-	preload("uid://frlgke42e0co"),
-	preload("uid://c4dsbnxccrcua"),
-	preload("uid://bpuj4a2rfn0r7"),
-	preload("uid://fr3iw5cj7rlw"),
-	preload("uid://b2mrjvgcjv6m8"),
-	preload("uid://0mlgutjd6l66")
+	preload("uid://cpn11df043lg1"),
+	preload("uid://bju44v2wcw73l"),
+	preload("uid://c7xwmm8jdmr21"),
+	preload("uid://daee4adah8nbw"),
+	preload("uid://bmsj8dydna4vq"),
+	preload("uid://bnnn042o2bicl"),
+	preload("uid://x1vgqivcu7tm"),
+	preload("uid://byxu76405uqkq")
 ]
 
 const _CHAR_ANONYMOUS: CompressedTexture2D = preload("uid://qomxhu5es3gm")
